beb982934580e7af3ef9a86cfa872649be5a4878
Luís Gustavo Verri Zacheu <luiszacheu@gmail.com>
2017-02-01 22:12:13 -0200

    config routes to redirect admin_rails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
-    app_dashboard_index_path
+    rails_admin_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   get 'welcome/index'
+  get '/dashboard' => "dashboard#index", :as => :dashboard
 
   root 'welcome#index'
 
